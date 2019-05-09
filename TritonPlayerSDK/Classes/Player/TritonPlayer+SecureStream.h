82f8a47d15e8cf9dc7d2f6b807e49517acdf290d
JD Buys <jd@spacial.com>
2019-05-09 11:08:58 +0200

    Remove SecureStream dependency

--- TritonPlayerSDK/Classes/Player/TritonPlayer+SecureStream.h
+++
@@ -1,8 +0,0 @@
-#import "TritonPlayer.h"
-#import "SecureStruct.h"
-
-@interface TritonPlayer (SecureStream)
-
-- (void)initSecurity:(SecureStruct*)pSecInfo;
-
-@end
--- TritonPlayerSDK/Classes/Player/TritonPlayer+SecureStreamPrivate.h
+++
@@ -1,8 +0,0 @@
-#import "TritonPlayer.h"
-#import "SecureStruct.h"
-
-@interface TritonPlayer (SecureStreamPrivate)
-
-- (NSString*)processChallengeWithCode:(NSString*)codeString andChallenge:(NSString*)challengeString;
-
-@end
